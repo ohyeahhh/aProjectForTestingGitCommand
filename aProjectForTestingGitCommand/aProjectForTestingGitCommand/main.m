45725e4b7f4da9d0bab0b27e51888f5908e8533b
myself <ohyeahmisoh@gmail.com>
2016-11-22 12:49:04 +0800

    create a project into the repository

---
+++ aProjectForTestingGitCommand/aProjectForTestingGitCommand/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  aProjectForTestingGitCommand
+//
+//  Created by mac on 16/11/22.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
